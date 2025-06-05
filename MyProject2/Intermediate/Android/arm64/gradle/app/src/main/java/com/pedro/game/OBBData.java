eefd3c5713f114cb4038d65145376ba45ac60c9b
intership <internship@ggtech.gg>
2025-06-05 14:15:49 +0200

    Definitivo

--- MyProject2/Intermediate/Android/arm64/gradle/app/src/main/java/com/pedro/game/OBBData.java
+++ MyProject2/Intermediate/Android/arm64/gradle/app/src/main/java/com/pedro/game/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-270338011L // the length of the file in bytes
+270319247L // the length of the file in bytes
 )
 };
 };
